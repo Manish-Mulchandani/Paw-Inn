f9c93f497706f3c83e601ece6bcad11011f73114
Manish Mulchandani <manishm9730@gmail.com>
2021-08-26 21:06:10 +0530

    react pet hotel

---
+++ src/pages/Home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import {Link} from "react-router-dom";
+import Services from "../components/Services";
+import FeaturedRooms from "../components/FeaturedRooms";
+
+
+export default function Home() {
+    return (
+    <>                     
+    <Hero>
+        <Banner title="Luxurious Hooms" subtitle="Deluxe room starting at $299">
+            <Link to="/rooms" className="btn-primary">
+                Our Rooms
+            </Link>
+        </Banner>
+    </Hero>
+    <Services/>
+    <FeaturedRooms />
+    </>
+    );
+}
+
+/*
+<Hero hero="defaultHero" />;
+Hero.defaultProps = {
+    hero: "defaultHero"
+};*/
